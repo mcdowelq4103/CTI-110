7008967e58834ca773b4da8ed7d1c5d465c41388
mcdowelq4103 <mcdowelq4103@student.faytechcc.edu>
2026-09-23 13:21:04 -0400

    First Python Assignment
    taking input from user and printing output

---
+++ P1LAB1_McDowellQualynn.py
@@ -0,0 +1,8 @@
+# Qualynn McDowell
+# p1lab1.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " ,first_name, last_name +"!", "Welcome to CTI-110.")
